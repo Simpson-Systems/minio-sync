de34270ada1d7426d0bab6440d157fa27128e6ac
Lucas Simpson <lucassimpson0213@gmail.com>
2026-03-22 02:11:12 -0400

    added more buckets

--- mirror.sh
+++ mirror.sh
@@ -6,4 +6,9 @@ mc alias set hawkbackup http://100.73.137.94:9100 minioadmin minioadmin
 mc mirror --watch tigerlocal/durable hawkbackup/durable &
 mc mirror --watch tigerlocal/databasus-backups hawkbackup/databasus-backups &
 
+mc mirror --watch tigerlocal/incoming-raw hawkbackup/incoming-raw &
+mc mirror --watch tigerlocal/normalized-events  hawkbackup/normalized-events &
+mc mirror --watch tigerlocal/artifacts hawkbackup/artifacts &
+mc mirror --watch tigerlocal/dead-letter hawkbackup/dead-letter &
+
 wait
